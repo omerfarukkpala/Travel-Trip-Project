c0f43dd0c9a4077b974b1d8789bf0f673ce0536a
= <omerfarukkpala@gmail.com>
2024-01-04 11:02:34 +0300

    .

---
+++ MvcTravel/MvcTravelTrip/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using MvcTravelTrip.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTravelTrip.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About Controller
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Abouts.ToList();
+            return View(values);
+        }
+    }
+}
